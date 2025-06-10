e6733a0bb1c3b83659c852bf88106aa3388eaf3c
Peng Chen <383559698@qq.com>
2025-06-10 13:56:56 +0800

    update App

--- composeApp/src/wasmJsMain/kotlin/simple/github/sponsor/App.kt
+++ composeApp/src/wasmJsMain/kotlin/simple/github/sponsor/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -14,6 +15,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -25,7 +27,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
@@ -64,25 +69,39 @@ fun App() {
         }
 
         Box(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(10.dp),
             contentAlignment = Alignment.Center
         ) {
             if (imageList.isEmpty()) {
                 CircularProgressIndicator()
             } else {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(3),
-                    modifier = Modifier.fillMaxWidth(0.5f)
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
                         .fillMaxHeight(),
-                    horizontalArrangement = Arrangement.spacedBy(2.dp),
-                    verticalArrangement = Arrangement.spacedBy(2.dp),
+                    verticalArrangement = Arrangement.spacedBy(20.dp),
                 ) {
-                    items(imageList) { url ->
-                        AsyncImage(
-                            model = url,
-                            contentDescription = "image",
-                            modifier = Modifier.fillMaxWidth()
-                        )
+                    Text(
+                        text = "Thank you for your support !",
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 45.sp,
+                        fontFamily = FontFamily.Cursive
+                    )
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(3),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        horizontalArrangement = Arrangement.spacedBy(2.dp),
+                        verticalArrangement = Arrangement.spacedBy(2.dp),
+                    ) {
+                        items(imageList) { url ->
+                            AsyncImage(
+                                model = url,
+                                contentDescription = "image",
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
                     }
                 }
             }
